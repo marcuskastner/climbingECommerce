527f68c97cd49a6d35884252d2a0197932f56866
Marcus Kastner <marcushkastner@gmail.com>
2022-07-05 12:38:22 -0700

    fourth try

--- web/src/components/molecule/NavBar.js
+++ web/src/components/molecule/NavBar.js
@@ -10,15 +10,19 @@ import SideBarMenu from "../atom/SideBarMenu"
 import Cart from "./Cart"
 function NavBar() {
   const { showCart, setShowCart, isOpen, toggleOpen } = useStateContext()
-  const [isDesktop, setDesktop] = useState(window.innerWidth > 650)
+  const [isDesktop, setDesktop] = useState(true)
 
   const updateMedia = () => {
-    setDesktop(window.innerWidth > 650)
+    if (typeof window !== `undefined`) {
+      setDesktop(window.innerWidth > 650)
+    }
   }
 
   useEffect(() => {
-    window.addEventListener("resize", updateMedia)
-    return () => window.removeEventListener("resize", updateMedia)
+    if (typeof window !== `undefined`) {
+      window.addEventListener("resize", updateMedia)
+      return () => window.removeEventListener("resize", updateMedia)
+    }
   })
 
   // On resize checks whether screen width is above 650, if it is above 650 isDesktop will be set to true
